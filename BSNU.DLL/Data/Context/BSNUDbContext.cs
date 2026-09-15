301aa93b2382fb7dfe06eb88b15e7872ae0dfccc
yahia <yahia@ZIIKO>
2026-09-15 13:37:18 +0300

    Add project files.

---
+++ BSNU.DLL/Data/Context/BSNUDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BSNU.DLL.Data.Enities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSNU.DLL.Data.Context
+{
+    public class BSNUDbContext : DbContext
+    {
+        public BSNUDbContext(DbContextOptions<BSNUDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<News> News { get; set; }
+    }
+
+
+}
---
+++ BSNU.DLL/Data/Enities/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSNU.DLL.Data.Enities
+{
+    public class News : BaseEntite
+    {
+        public string Title { get; set; } 
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public string Type { get; set; }
+    }
+}
